604d764d5ff3004ead9340fd729bdb3a9fa0e4b9
Badr9907 <awlhajb@gmail.com>
2025-09-25 06:43:39 +0100

    Update homehandler.go

--- handlers/homehandler.go
+++ handlers/homehandler.go
@@ -5,32 +5,37 @@ import (
 	"net/http"
 )
 
-// handling homepage request
+type IndexPageData struct {
+	Artists []Artist
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/"{
-		HandleError(w,"404 not found",http.StatusNotFound)
+	if r.URL.Path != "/" {
+		HandleError(w, "404 not found", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" {
 		HandleError(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	// fetching artistes then parsing files
 	artists, err := FetchArtists()
 	if err != nil {
 		HandleError(w, "Failed to fetch artists", http.StatusInternalServerError)
 		return
 	}
 
-	tmpl ,err:= template.ParseFiles("templates/index.html")
-	if err != nil{
-		HandleError(w,"can't access this file ",http.StatusInternalServerError)
+	filter := ParseFilters(r.URL.Query())
+	filtered := FilterArtists(artists, filter, nil)
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		HandleError(w, "can't access this file", http.StatusInternalServerError)
 		return
 	}
-	err1 := tmpl.Execute(w, artists)
-	if err1!=nil{
-		HandleError(w,"Failed to show artists",http.StatusInternalServerError)
-
+	data := IndexPageData{
+		Artists: filtered,
 	}
-	
+	if err := tmpl.Execute(w, data); err != nil {
+		HandleError(w, "Failed to show artists", http.StatusInternalServerError)
+	}
 }
